07876b38e3a775276a2c676e1cae49a36d751de5
Amanda Abrao <amandaabrao55@gmail.com>
2017-05-30 12:38:32 -0300

    primeiro commit

---
+++ Assets/Scripts/CutSceneEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneEvents : MonoBehaviour
+{
+
+    public void CameraAcabou()
+    {
+        CutSceneController.instancia.ProximaCamera();
+    }
+
+
+}
